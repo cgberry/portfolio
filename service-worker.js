143a3b2d33ccb4b52d0cd6833364f5ff3b3e67ea
cgberry@asu.edu <cgberry@asu.edu>
2019-12-30 18:54:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2857bd048cc22c33cd99bb5ba406783f.js"
+  "/portfolio/precache-manifest.2adddaf905255904f19f5c0e027efd98.js"
 );
 
 self.addEventListener('message', (event) => {
